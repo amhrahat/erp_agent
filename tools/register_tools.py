ee3c032c934b21d6408847d6500044bd818c2ba6
amhrahat <rahatabdullah879@gmail.com>
2026-04-12 20:02:24 +0600

    add gemini

--- tools/register_tools.py
+++ tools/register_tools.py
@@ -2,7 +2,8 @@ from langchain_core.tools import StructuredTool
 from erp.frappe_client import erp
 from schemas.customer_schema import InsertCustomerInput
 from tools.erp_tools.customer_tools import insert_customer_tool
-from llms.ollama_client import llm
+# from llms.ollama_client import llm
+from llms.gemini_client import llm
 
 tools = [insert_customer_tool]
 
